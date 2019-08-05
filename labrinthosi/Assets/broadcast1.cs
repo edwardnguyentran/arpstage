42f138db625690dc5f2678c2529b3806c490d84a
Edward <edward.tran.nguyen@gmail.com>
2019-08-06 01:02:39 +1000

    DHCP step transitions

---
+++ labrinthosi/Assets/broadcast1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class broadcast1 : MonoBehaviour
+{
+    public TextMeshProUGUI errortext;
+    public TextMeshProUGUI title1;
+    public TextMeshProUGUI title2;
+    public TextMeshProUGUI title3;
+    public TextMeshProUGUI title4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        title1.enabled = false;
+        title2.enabled = false;
+        title3.enabled = false;
+        title4.enabled = false;
+        errortext.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        errortext.GetComponent<TextMeshProUGUI>();
+        title1.GetComponent<TextMeshProUGUI>();
+        title2.GetComponent<TextMeshProUGUI>();
+        title3.GetComponent<TextMeshProUGUI>();
+        title4.GetComponent<TextMeshProUGUI>();
+    }
+
+    public void broadcastClick()
+    {
+        errortext.GetComponent<TextMeshProUGUI>();
+        title1.GetComponent<TextMeshProUGUI>();
+        title2.GetComponent<TextMeshProUGUI>();
+        title3.GetComponent<TextMeshProUGUI>();
+        title4.GetComponent<TextMeshProUGUI>();
+
+        if(title1.enabled == false && title2.enabled == false && title3.enabled == false && title4.enabled == false)
+        {
+            title1.enabled = true;
+            errortext.enabled = false;
+        }
+        else
+        {
+            errortext.enabled = true;
+        }
+    }
+}
